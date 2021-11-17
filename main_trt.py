c42b14fe1d2067c501ddce906ce968e6282c44e3
atroncoso2 <atroncoso2@uc.cl>
2021-11-17 13:13:25 -0300

    main trt for onnx creation

--- main_trt.py
+++ main_trt.py
@@ -1,10 +1,13 @@
 import numpy as np
 import argparse
 
-from trt_lib.Interface import Interface
+from trt_lib.Interface import Interface as trtInterface
 from importable_main import import_data
 from data_worker.data_worker import split_into_batches
 from torch_lib.data_worker import suit4torch
+from torch_lib.Interface import Interface as torchInterface
+from torch_lib.Nets import LargeNet as torchLNet, \
+    MediumNet as torchMNet, SmallNet as torchSNet
 
 
 def get_args():
@@ -29,25 +32,25 @@ def get_args():
     parser.add_argument(
         '-bs', '--batch_size', help='select batch size', type=int, default=10)
     args = parser.parse_args()
-    return args
+    return vars(args)
 
 
-def export_onnx_main():
-    pass
+def export_onnx_main(path, size, framework, dummy_input, **kwargs):
+    net = torchSNet()
+    if size == 'small_v1':
+        net = torchSNet()
+    elif size == 'medium_v1':
+        net = torchMNet()
+    elif size == 'large_v1':
+        net = torchLNet()
+    net_interface = torchInterface(net)
+    net_interface.load_weights(
+        f'saved_nets/saved_{framework}/{size}.pth')
+
+    net_interface.convert2onnx(path, dummy_input)
 
 
 def run_trt_main():
-    pass
-
-
-if __name__ == "__main__":
-
-    args = get_args()
-
-    if args.export_onnx:
-        print('export onnx')
-    else:
-        print('normal')
 
     target_dtype = np.float32
 
@@ -67,10 +70,26 @@ if __name__ == "__main__":
 
     path = "saved_nets/saved_onnx/torch_small_v1.trt"
 
-    net_interface = Interface(
+    net_interface = trtInterface(
         path, X, batch_size=batch_size, n_classes=10,
         target_dtype=target_dtype)
 
     Y_pred = net_interface.predict_net(X)
 
     print(Y_pred)
+
+
+if __name__ == "__main__":
+
+    kwargs = get_args()
+
+    X_data, Y_data = import_data()
+    X, Y = suit4torch(X_data, Y_data)
+    batch_size = kwargs.batch_size
+    batches = split_into_batches(X, Y, batch_size)
+    dummy_batch, _ = batches[0]
+
+    if kwargs.export_onnx:
+        export_onnx_main(dummy_input=dummy_batch, **kwargs)
+    else:
+        run_trt_main(**kwargs)
